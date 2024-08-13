e9fdce680b9a072bcbb08f5f4a7539743a9950c6
EmersonBarranco <desingf19@gmail.com>
2024-08-12 23:22:14 -0500

    feat: Se agrego el directorio de front

---
+++ front/bank/src/app/shared/components/loading-spinner/loading-spinner.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-loading-spinner',
+  standalone: true,
+  imports: [],
+  templateUrl: './loading-spinner.component.html',
+  styles: ``
+})
+export class LoadingSpinnerComponent {
+
+}
